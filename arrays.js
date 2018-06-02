a7ac1e2908337d99c094b6af80080e2b56a08684
Corey H <c.l.halicke@gmail.com>
2018-06-02 00:49:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,6 +20,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  var array = [1, 2, 3]
+  array = [1, 2, 3]
   return array[2]
 }
+
+function 
